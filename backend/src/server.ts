d4c3b709069944ea9f0bad22dbae7da2494fade4
Lucas Henrique <lucashenriquevieira86@gmail.com>
2026-06-24 13:15:14 -0300

    feat: add start function at server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,19 +1,22 @@
 import Fastify from 'fastify'
 import cors from '@fastify/cors'
 
-const app = Fastify({
-    logger: true
-})
+const app = Fastify()
 const port: number = 3333
 
 app.get('/', async () => {
     return { msg: "Hello World"}
 })
 
-try {
-    app.listen(({port}), () => {
-        console.log(`Servidor rodando em http://localhost:${port}`)
-    })
-} catch (error) {
-    throw new Error()
+const start = async () => {
+    try {
+        await app.listen(({port}), () => {
+            console.log(`Servidor rodando em http://localhost:${port}`)
+        })
+    } catch (error) {
+        app.log.error(error)
+        process.exit(1)
+    }
 }
+
+start()
